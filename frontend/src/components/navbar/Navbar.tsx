dbc7d439b209287f0a5aff87c4a958980fce8bdf
ulrich-742 <ulrichamoussoupro@gmail.com>
2026-07-13 16:51:00 +0200

    navbar and color for site

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,9 +1,27 @@
-import "Navbar.css"
+import "./Navbar.css";
 
-export default function Navbar(){
-    return(
-        <nav>
-            <h1>run</h1>
-        </nav>
-    )
+export default function Navbar() {
+	return (
+		<nav className="navbar">
+			<div className="navbar__container">
+				<div className="navbar__brand">
+					<span className="navbar__icon">🐍</span>
+
+					<span className="navbar__title">
+						Snake Game
+					</span>
+				</div>
+
+				<div className="navbar__menu">
+					<div className="navbar__item">
+						Jeu
+					</div>
+
+					<div className="navbar__item">
+						Classement
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 }
---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../../components/navbar/Navbar";
+import "./Home.css";
+
+export default function Home() {
+	return (
+		<div className="home">
+			<Navbar />
+		</div>
+	);
+}

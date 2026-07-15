f11b66f522675f0b725a8259be41399aaf0d79db
ulrich-742 <ulrichamoussoupro@gmail.com>
2026-07-15 10:37:57 +0200

    footer

---
+++ frontend/src/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+export default function Footer() {
+	return (
+		<footer className="footer">
+			<div className="footer__container">
+				<p className="footer__text">© 2026 Snake Game</p>
+
+				<p className="footer__text">BY ulrich-742 git</p>
+			</div>
+		</footer>
+	);
+}
